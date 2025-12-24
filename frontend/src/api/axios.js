5a80054505b29e6af7acf56e14d2fd240aeeca65
Prakashray-dev <prakashrayias@gmail.com>
2025-12-24 19:45:56 +0530

    fix axios baseURL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5000/api"
-      : import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
