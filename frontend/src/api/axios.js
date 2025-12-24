353254bce2852b9d4eef6024e115bc38aa919c4e
Prakashray-dev <prakashrayias@gmail.com>
2025-12-24 18:51:11 +0530

    frontend deployment ready

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
 });
 
 export default axiosInstance;
